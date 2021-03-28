66fb5d63893d0ec8a788b189a1e94127e18d201f
Andrii Hanzha <ganzha_as@outlook.com>
2021-03-28 18:22:56 +0300

    Destroyed instances

--- terraform/Lesson1/main.tf
+++ terraform/Lesson1/main.tf
@@ -3,13 +3,3 @@ provider "aws" {
   secret_key = "oTyFCl1QA/mZj3da1uZ/WMYjTOqXCyfZ1igcDxQY"
   region = "eu-central-1"
 }
-
-resource "aws_instance" "sample" {
-  count = 3
- ami = "ami-0767046d1677be5a0"
- instance_type = "t2.micro"
- tags = {
-   Name = "My Ubuntu servers"
-   Owner = "Andrii"
- }
-}
